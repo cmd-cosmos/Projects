2d43517ff31456d4aa8f6c52077fe7614bb30389
Premanshu Bhagat <premtsapj@gmail.com>
2025-06-25 20:51:37 -0400

    initial commit

---
+++ C/TCP-Server/sockets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+#define IP "142.251.41.35" // www.google.ca
+#define PORT 80 // HTTP port specifier
+
+int main(void)
+{
+    int s;
+    socket_address_in socket;
+
+    s = socket
+    return 0;
+}
